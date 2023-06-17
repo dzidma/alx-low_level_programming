b14f58dd4514329a3b1136baf92fdbfc530498e3
dzidma <20madzid@gmail.com>
2023-06-16 17:42:31 -0700

    finish

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Description: C program that prints with printf function
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
