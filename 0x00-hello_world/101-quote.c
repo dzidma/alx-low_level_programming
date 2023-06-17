a8959518fd70bb7a24dce08cdcb1b040f76506c5
dzidma <20madzid@gmail.com>
2023-06-16 18:24:21 -0700

    seffe

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ *  Description: C program that prints
+ *
+ * Return: 1 (not Success)
+*/
+int main(void)
+{	char sent = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, sent, 59);
+	return(1);
+}	
