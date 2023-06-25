b48037819bba1a6162f329bbbbd3492d8d40ea7e
dzidma <20madzid@gmail.com>
2023-06-25 07:36:22 -0700

    69

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible different combinations of two digits
+ *              using the putchar function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, a;
+	int c = 9; /**/
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = i + 1; a < 10; a++)
+		{
+			putchar('0' + i);
+			putchar('0' + a);
+			if (i == 8 && a == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
