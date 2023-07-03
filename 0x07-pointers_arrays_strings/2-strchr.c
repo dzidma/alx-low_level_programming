fdc9d3d935107324a386322721f5743c377ef255
dzidma <20madzid@gmail.com>
2023-07-03 13:35:43 -0700

    ja

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,12 @@
 char *_strchr(char *s, char c)
 {
 	int itr;
-    
-    for (itr = 0; s[itr] >= '\0' ; itr++)
-    {
-        if (s[itr] == c)
-        {
-            return (s + itr);
-        }
-    }
-    
-    return ('\0');
+
+	for (itr = 0; s[itr] != '\0'; itr++)
+	{
+		if (s[itr] == c)
+			return (s + itr);
+	}
+
+	return ('\0');
 }
