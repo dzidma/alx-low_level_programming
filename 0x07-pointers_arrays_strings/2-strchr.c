c0027611f4d9542ab6d8c57c65ef6aa11e42261a
dzidma <20madzid@gmail.com>
2023-07-03 13:26:16 -0700

    hah

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strchr - Locates the first occurrence of a character in a string
  * @s: Pointer to the string
@@ -12,17 +13,13 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 
 		s++;
 	}
 
 	if (c == '\0')
-	{
 		return (s);
-	}
 
 	return (NULL);
 }
