cccb47c7eca5078eb3c4a643eec660a0811b9873
dzidma <20madzid@gmail.com>
2023-10-26 07:38:20 -0700

    edf

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * factorial: function returns the factorial of a given number by recursion.
- * @n: number that its factorial should be calculated.
- * Return: either factorial value or -1 if failed.
+ *factorial- function calculates the factorial of a given number by recursion.
+ *@n: number that its factorial should be calculated.
+ *Return: either factorial value or -1 if failed.
 */
 
 int factorial(int n)
